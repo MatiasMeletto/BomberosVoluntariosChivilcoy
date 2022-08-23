080620f87374692af3a70679b24b4997e1bbd3a6
Lautaro <info-4@INFO4-PC2>
2022-08-23 10:39:22 -0300

    Creacion del objeto Servicios Especiales

---
+++ BlazorApp1/Data/Models/ServiciosEspeciales.cs
@@ -0,0 +1,20 @@
+﻿namespace BlazorApp1.Data.Models
+{
+    public class ServiciosEspeciales
+    {
+        public int ServiciosEspecilesId { get; set; }
+
+
+        public string InformacionServicio { get; set; }
+
+
+        public string TipoRepresentacion { get; set; }
+
+
+        public string TipoPrevencion { get; set; }
+
+
+        public int DatosCapacitacionId { get; set; }
+        public DatosCapacitacion DatosCapacitacion { get; set; }
+    }
+}
