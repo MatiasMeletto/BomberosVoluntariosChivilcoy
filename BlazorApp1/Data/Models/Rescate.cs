e32785c5437b681c881976bdab0e60981aa98e51
Matias <info-4@INFO4-PC3>
2022-08-23 10:42:45 -0300

    Creacion de Rescate

---
+++ BlazorApp1/Data/Models/Rescate.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorApp1.Data.Models
+{
+    public class Rescate
+    {
+        public int RescateId { get; set; }
+
+
+        public int LugarId { get; set; }
+        public Lugar Lugar { get; set; }
+         
+        public int SalidaId { get; set; }
+        public Salida Salida { get; set; }
+    }
+}
