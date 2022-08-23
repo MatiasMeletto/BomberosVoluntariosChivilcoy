6ee6a0ad924e448b9f9b2656a72f692807d71067
Matias <info-4@INFO4-PC3>
2022-08-23 10:23:55 -0300

    Creacion del objeto accidente

---
+++ BlazorApp1/Data/Models/Accidente.cs
@@ -0,0 +1,19 @@
+﻿namespace BlazorApp1.Data.Models
+{
+    public class Accidente
+    {
+        public int AccidenteId { get; set; }
+
+        public int CantidadVehiculos { get; set; }
+
+
+        public List<Vehiculo> Vehiculos { get; set; }
+
+
+        public string CondicionesClimaticas { get; set; }
+
+
+        public int SalidaId { get; set; }
+        public Salida Salida { get; set; }
+    }
+}
