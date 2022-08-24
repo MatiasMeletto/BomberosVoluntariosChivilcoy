b2c1415da41cff587ac4e6fff3ce496ff17ccd4b
Tobias <info-4@INFO4-PC4>
2022-08-24 10:56:31 -0300

    Contacto

---
+++ BlazorApp1/Data/Models/Contacto.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorApp1.Data.Models
+{
+    public class Contacto
+    {
+        public int ContactoId { get; set; }
+        public int BomberoId { get; set; }
+        public  Bombero Bombero { get; set; }       
+        public string? TelefonoCel { get; set; }
+        public string? TelefonoLaboral { get; set; }
+        public string? TelefonoFijo { get; set; }
+        public string? Email { get; set; }
+    }
+}
