0e3fd2f7d95cec69046bfd75eb9796728e8ae8c4
Matias <info-4@INFO4-PC3>
2022-08-24 09:15:33 -0300

    arreglos

--- BlazorApp1/Data/Models/Incendio.cs
+++ BlazorApp1/Data/Models/Incendio.cs
@@ -7,15 +7,16 @@ namespace BlazorApp1.Data.Models
         public int SuperficieAccidenteId { get; set; }
 
         public int LugarId { get; set; }
+        public Lugar Lugar { get; set; }
 
-        public string? Lugar { get; set; }
+        public bool DeteccionAutomaticaId { get; set; }
 
-        public string? DeteccionAutomaticaId { get; set; }
-
-        public string? ExtincionId { get; set; }
-
-        public int VehiculosAfectados { get; set; }
-
+        public bool Extintor { get; set; }
+        public bool Hidrante { get; set; }
+        public int EmbarcacionAfectadaId { get; set; }
+        public EmbarcacionAfectada EmbarcacionAfectada { get; set; }
+        public int VehiculoAfectadoId { get; set; }
+        public VehiculoAfectado VehiculoAfectado { get; set; }
         public string? LugarDeSiniestro { get; set; }
 
         public TipoIncendio Tipo { get; set; }
