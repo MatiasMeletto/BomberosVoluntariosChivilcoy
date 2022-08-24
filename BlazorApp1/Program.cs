114d9f59cbbf4ef1e0d9d2974b7291c472d8ecd0
Danilo Gariboldi <dgariboldi@eest1chivilcoy.edu.ar>
2022-08-24 10:58:19 -0300

    Cambios y pruebas

--- BlazorApp1/Program.cs
+++ BlazorApp1/Program.cs
@@ -8,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddBlazorStrap();
+builder.Services.AddAntDesign();
 
 var app = builder.Build();
 
