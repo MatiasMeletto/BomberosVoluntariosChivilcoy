25a53720ed3e641bb1efd579a8d54fc6c76bf3f9
Matias <info-4@INFO4-PC3>
2022-08-24 10:40:56 -0300

    Agregado de persona y bombero

---
+++ BlazorApp1/Data/Models/Bombero.cs
@@ -0,0 +1,26 @@
+﻿namespace BlazorApp1.Data.Models
+{
+    public class Bombero : Persona
+    {
+        public int BomberoId { get; set; }
+
+        public int NumeroLegajo { get; set; }
+        public string Estado { get; set; }
+        public DateTime FechaAceptacion { get; set; }
+        public string Dotacion { get; set; }
+        public string Brigada { get; set; }
+        public string Resolucion1 { get; set; }
+        public string Resolucion2 { get; set; }
+        public string Resolucion3 { get; set; }
+        public string Resolucion4 { get; set; }
+        public string Resolucion5 { get; set; }
+        public string Resolucion6 { get; set; }
+        public bool Chofer { get; set; }
+        public DateTime? VencimientoRegistro { get; set; }
+
+        public List<Vehiculo>? Vehiculos { get; set; } = new();
+       
+        public int? MovilId { get; set; }
+        public Movil? Movil { get; set; }
+    }
+}
---
+++ BlazorApp1/Data/Models/Persona.cs
@@ -0,0 +1,21 @@
+﻿namespace BlazorApp1.Data.Models
+{
+    public class Persona
+    {
+        public int PersonaId { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Direccion { get; set; }
+        public string LugarNacimiento { get; set; }
+        public string Documento { get; set; }
+        public string GrupoSanguineo { get; set; }
+        public string Observaciones { get; set; }
+        public string Profesion { get; set; }
+        public string NivelEstudios { get; set; }
+        public string NumeroIoma { get; set; }
+
+        public int ContactoId { get; set; }
+        public Contacto Contacto { get; set; }
+    }
+}
