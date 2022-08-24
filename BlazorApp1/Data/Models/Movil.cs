7989853f3d2bbb626b528463f623eb8af86d4b45
lautaro <info-4@INFO4-PC2>
2022-08-24 10:41:33 -0300

    Creacion de la clase movil y clase vehiculo

---
+++ BlazorApp1/Data/Models/Movil.cs
@@ -0,0 +1,18 @@
+﻿namespace BlazorApp1.Data.Models
+{
+    public class Movil
+    {
+        public string NroMovil { get; set; }
+        public string NroMotor { get; set; }
+        public string NroChasis { get; set; }
+        public string Encargado { get; set; }
+        public string Estado { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public int Año { get; set; }
+        public string Patente { get; set; }
+        public string Tipo { get; set; }
+        public int BomberoId { get; set; }
+        public Bombero Bombero { get; set; }
+    }
+}
---
+++ BlazorApp1/Data/Models/Vehiculo.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorApp1.Data.Models
+{
+    public class Vehiculo
+    {
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public int Año { get; set; }
+        public string Patente { get; set; }
+        public string Tipo { get; set; }
+        public int BomberoId { get; set; }
+        public Bombero Bombero { get;  set; }
+    }
+}
