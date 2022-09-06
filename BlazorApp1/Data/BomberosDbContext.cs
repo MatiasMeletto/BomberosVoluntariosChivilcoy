027db2e1768d1653e1e7330821abad0e8dab0423
Tobias <tcorrea@eest1chivilcoy.edu.ar>
2022-09-06 10:49:21 -0300

    Update Dbcontext

--- BlazorApp1/Data/BomberosDbContext.cs
+++ BlazorApp1/Data/BomberosDbContext.cs
@@ -7,9 +7,37 @@ namespace BlazorApp1.Data
 {
     public class BomberosDbContext : DbContext
     {
+        DbSet<Persona> Personas { get; set; }
+        DbSet<Bombero> Bomberos { get; set; }
+        DbSet<Movil> Moviles { get; set; }
+        DbSet<MovilBombero> MovilesBomberos { get; set; }
+        DbSet<VehiculoPersonal> VehiculosPersonales { get; set; }
         DbSet<Contacto> Contactos { get; set; }
+        DbSet<Salida> Salidas { get; set; }
         DbSet<Damnificado> Damnificados { get; set; }
         DbSet<Seguro> Seguros { get; set; }
+        DbSet<DatosCapacitacion> DatosCapacitaciones { get; set; }
+        DbSet<EmbarcacionAfectada> EmbarcacionesAfectadas { get; set; }
+        DbSet<VehiculoAfectado> VehiculosAfectados { get; set; }
+        DbSet<VehiculoAfectadoAccidente> VehiculosAfectadosAccidentes { get; set; }
+        DbSet<VehiculoAfectadoIncendio> VehiculosAfectadoIncendios { get; set; }
+        DbSet<VehiculoDamnificado> VehiculosDamnificados { get; set; }
+        DbSet<Accidente> Accidentes { get; set; }
+        DbSet<Incendio> Incendios { get; set; }
+        DbSet<Rescate> Rescates { get; set; }
+        DbSet<FactorClimatico> FactoresClimaticos { get; set; }
+        DbSet<RescatePersona> RescatePersonas { get; set; }
+        DbSet<RescateAnimal> RescateAnimales { get; set; }
+        DbSet<IncendioComercio> IncendiosComercios { get; set; }
+        DbSet<IncendioEstablecimientoEducativo> IncendiosEstablecimientosEducativos { get; set; }
+        DbSet<IncendioEstablecimientoPublico> IncendiosEstablecimientosPublicos { get; set; }
+        DbSet<IncendioForestal> IncendiosForestales { get; set; }
+        DbSet<IncendioHospitalesYClinicas> IncendiosHospitalesYClinicas { get; set; }
+        DbSet<IncendioIndustria> IncendiosIndustrias {get; set; }
+        DbSet<IncendioVivienda> IncendiosViviendas { get; set; }
+        DbSet<MaterialesPeligrosos> MaterialesPeligrosos { get; set; }
+        DbSet<ServicioEspecial> ServicioEspeciales { get; set; }
+
 
         public BomberosDbContext(DbContextOptions<BomberosDbContext> options)
             : base(options)
