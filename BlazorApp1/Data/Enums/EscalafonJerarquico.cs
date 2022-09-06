02667dba2fefdf75eba46cfffaac9ebf8a97b2e9
Tobias <tcorrea@eest1chivilcoy.edu.ar>
2022-09-06 11:03:38 -0300

    Creacion Escalafon Jerarquico

---
+++ BlazorApp1/Data/Enums/EscalafonJerarquico.cs
@@ -0,0 +1,19 @@
+﻿namespace BlazorApp1.Data.Enums
+{
+    public enum EscalafonJerarquico
+    {
+        Jefe,
+        SegundoJefe,
+        PrimerOficial,
+        SegundoOficial,
+        TercerOficial,
+        AuxiliarMayor,
+        PrimerAuxiliar,
+        SegundoAuxiliar,
+        Ayudante,
+        SegundoAyudante,
+        Bombero,
+        Aspirante,
+
+    }
+}
