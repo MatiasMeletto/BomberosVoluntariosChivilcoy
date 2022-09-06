6b6fecd5c9eaf89b9ec997139a4f783b3e96eef3
Tobias <tcorrea@eest1chivilcoy.edu.ar>
2022-09-06 10:15:29 -0300

    Update Dbcontext

--- BlazorApp1/Data/BomberosDbContext.cs
+++ BlazorApp1/Data/BomberosDbContext.cs
@@ -1,10 +1,16 @@
 ﻿using BlazorApp1.Data.Models.Personales;
+using BlazorApp1.Data.Models.Salidas.Componentes;
+using BlazorApp1.Data.Models.Salidas.Planillas;
 using Microsoft.EntityFrameworkCore;
 
 namespace BlazorApp1.Data
 {
     public class BomberosDbContext : DbContext
     {
+        DbSet<Contacto> Contactos { get; set; }
+        DbSet<Damnificado> Damnificados { get; set; }
+        DbSet<Seguro> Seguros { get; set; }
+
         public BomberosDbContext(DbContextOptions<BomberosDbContext> options)
             : base(options)
         {
